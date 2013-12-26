46039d6ebfcf2ff20b4edb4636cb972682cf6af4
Devin Sevilla <dasevilla@gmail.com>
2013-12-26 10:30:05 -0800

    Clean-up setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 with open('README.rst') as fp:
-    readme = fp.read()
+    long_description = fp.read()
 
-packages = [
-    'roviclient'
-]
-
-requires = [
-    'requests>=1.2.0'
-]
 
 setup(
     name='roviclient',
     version='0.1.0',
+
+    description='A simple Python client library for the Rovi API',
+    long_description=long_description,
+
     author='Devin Sevilla',
     author_email='dasevilla@gmail.com',
+
     url='https://github.com/dasevilla/rovi-python',
-    license='MIT',
-    description='A simple Python client library for the Rovi API',
-    long_description=readme,
-    install_requires=requires,
-    packages=packages,
+    download_url='https://github.com/dasevilla/rovi-python/tarball/master',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    install_requires=[
+        'requests>=1.2.0',
+    ],
+
+    packages=find_packages(),
 )
